519ade00e824b9abe3c701748ca5a5f50c59745c
Danny Tomkins <tomkinsdan@gmail.com>
2020-11-07 16:35:27 -0600

    added current weather traits and forecast cards

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var cityname
 var apiKey = "47d177e47bbc4e3cb8cd14f924ebac98"
-
+var searches =[]
+//searches.push(cityname)
 
 $("#search").on("click", function(){
    var cityname  = $("#cityName").val()
    searchWeather(cityname)
+    forecast(cityname)
 })
 
 
@@ -28,19 +30,46 @@ function searchWeather(cityname) {
         //temp, humidity, wind speed, uv index
         var spantemp=$("<span>")
         spantemp.text(results.main.temp)
+        var spanhumid=$("<span>")
+        spanhumid.text(results.main.humidity)
+        var spanwind=$("<span>")
+        spanwind.text(results.wind.speed)
+        // var spanuv=$("<span>")
+        // spanuv.text(results)
+
+        //uv index seperate API call
+        $.ajax({
+            type: "GET",
+            url: "https://api.openweathermap.org/data/2.5/uvi?lat="+results.coord.lat+"&lon="+results.coord.lon+"&appid=" + apiKey,
+        }).then(function (responseuv) 
+        {console.log(responseuv.value, "uv search")
+            $("#currentuv").append(responseuv.value)
+        })
 
         //add results from memory to page
         $("#citydate").append(spancity,spandate,weatherimg)
-        $("#currentemp").append(spantemp)
+        $("#currenttemp").append(spantemp)
+        $("#currenthumid").append(spanhumid)
+        $("#currentwind").append(spanwind)
+        
     })
+}
 
+function forecast(cityname) {
+    $.ajax({
+        type: "GET",
+        url: "https://api.openweathermap.org/data/2.5/forecast?q="+cityname+"&appid="+apiKey,
+    })
+        .then(function (forecastResults) {
+            console.log(forecastResults);
+            for (var i = 0; i < 5; i++){
 
-}   
-
-//api.openweathermap.org/data/2.5/weather?q={cityname}&appid={47d177e47bbc4e3cb8cd14f924ebac98}
-
-//create search bar
-//search onclick event
-//put name of city from search in API pull
-
-// Open Weather API key 47d177e47bbc4e3cb8cd14f924ebac98
+                var column = $("<div>").addClass("col-sm-2");
+                var card = $("<div>").addClass("card");
+                var cardBod = $("<div>").addClass("card-body");
+                var date = $("<h6>").addClass("card-title").text(forecastResults.list[i].dt_txt)
+                    var temp = $("<p>").text(forecastResults.list[i].main.temp)
+                $("#forecast").append(column.append(card.append(cardBod.append(date,temp))))
+                }
+})
+}
